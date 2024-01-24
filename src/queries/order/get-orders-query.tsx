cfefdb1a124168d60a8d16257259aae3c17aa7a7
Trieu Tien Nguyen <nguyenthanhtung111xxx@gmail.com>
2024-01-24 11:15:07 +0700

    update one

--- src/queries/order/get-orders-query.tsx
+++ src/queries/order/get-orders-query.tsx
@@ -10,8 +10,8 @@ export const useGetOrders = () => {
     queryKey: constructorGetOrders(),
     queryFn: getOrders,
     refetchOnWindowFocus: false,
-    cacheTime: 12 * 60 * 60 * 1000, // 12 giờ
-    staleTime: 12 * 60 * 60 * 1000 // 12 giờ
+    cacheTime: 12 * 60 * 60 * 1000,
+    staleTime: 12 * 60 * 60 * 1000 
 
   });
 };
